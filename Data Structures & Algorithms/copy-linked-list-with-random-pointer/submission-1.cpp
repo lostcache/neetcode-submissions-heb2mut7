f85c09cd4828d00ac677ebade5b7040f2a6ca9a5
Harsh Devisha <harshdevisha9111@gmail.com>
2026-04-15 20:49:27 -0700

    Add: copy-linked-list-with-random-pointer - submission-1

---
+++ Data Structures & Algorithms/copy-linked-list-with-random-pointer/submission-1.cpp
@@ -0,0 +1,59 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+*/
+
+class Solution {
+   public:
+    Node* copyRandomList(Node* head) {
+        if (!head) return nullptr;
+
+        unordered_map<Node*, Node*> m;
+
+        Node* headcpy = new Node(head->val);
+        m[head] = headcpy;
+
+        Node* curr = head;
+        Node* currcpy = headcpy;
+
+        while (curr) {
+            Node* randcpy = NULL;
+            if (curr->random) {
+                if (m.find(curr->random) != m.end()) {
+                    randcpy = m[curr->random];
+                } else {
+                    randcpy = new Node(curr->random->val);
+                    m[curr->random] = randcpy;
+                }
+            }
+            currcpy->random = randcpy;
+
+            Node* nextcpy = NULL;
+            if (curr->next) {
+                if (m.find(curr->next) != m.end()) {
+                    nextcpy = m[curr->next];
+                } else {
+                    nextcpy = new Node(curr->next->val);
+                    m[curr->next] = nextcpy;
+                }
+            }
+            currcpy->next = nextcpy;
+
+            curr = curr->next;
+            currcpy = currcpy->next;
+        }
+
+        return headcpy;
+    }
+};
