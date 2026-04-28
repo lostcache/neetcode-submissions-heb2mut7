04524559cfc0f1e1cf7d7f32760ebbb6469bcc2d
Harsh Devisha <harshdevisha9111@gmail.com>
2026-04-27 22:04:21 -0700

    Add: partition-equal-subset-sum - submission-4

---
+++ Data Structures & Algorithms/partition-equal-subset-sum/submission-4.cpp
@@ -0,0 +1,33 @@
+#include <stdint.h>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+  bool canPartition(vector<int> &nums) {
+    long long total = 0;
+    for (int i = 0; i < nums.size(); ++i) {
+      total += 1LL * nums[i];
+    }
+
+    if (total & 1) {
+      return false;
+    }
+
+    long long tar = total / 2;
+
+    vector<int> dp(5001, false);
+    dp[0] = true;
+
+    for (int i = 0; i < nums.size(); ++i) {
+      for (int j = tar; j >= nums[i]; --j) {
+        if (dp[j - nums[i]]) {
+          dp[j] = true;
+        }
+      }
+    }
+
+    return dp[tar];
+  }
+};
